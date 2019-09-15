71c69fa518b625b0351c25b68246dbcf1d2a6603
Tracy Reuther <tracyreuther@gmail.com>
2019-09-15 12:18:53 +0000

    Automatically backed up by Learn

--- db/migrate/20190915120731_create_users.rb
+++ db/migrate/20190915120731_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
      create_table :users do |t|
        t.string :username
        t.string :password_digest
+#password_digest is required for BCrypt to work. BUT the attribute still
+#equals password when using password in views and controller
      end
    end
 
